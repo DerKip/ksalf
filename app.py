6ae5cec346fb53de2ec8de8758936af912790523
Derrick Kipchumba <dkip64@gmail.com>
2018-06-05 20:37:45 +0300

    [ ft ] update login form

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request , redirect
-
+from flask import Flask, render_template, request , redirect, url_for, \
+flash
 app=Flask(__name__) #setting up our WSGI app from flask
 
 @app.route('/')
@@ -13,11 +13,14 @@ def welcome():
 @app.route('/login',methods=['GET','POST'])
 def login():
     error=None
+    
     if request.method == 'POST':
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error= 'Invalid creadentials, please try again'
+            error = 'Invalid creadentials, please try again'
         else:
-            return redirect((url_for('home'))
+            user=request.form['username']
+            message='logged in %s ' %user
+            return redirect(url_for('home',message=message))
     return render_template('login.html',error=error)
 
 
